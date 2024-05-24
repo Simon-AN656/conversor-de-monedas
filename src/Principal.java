1fd9d795101411f8d866db242d2b03796e8d4b47
Siwas666 <seimon656@gmail.com>
2024-05-23 21:20:19 -0600

    Se crea archivo .txt de documentacion de la API utilizada

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
-
+        //API key 77c6c01ad7eeeb6ea0e799ff
+        //GET Request API Pair https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP
     }
 }
