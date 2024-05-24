a05d1c657b84e3f5a2280e1fb4ae3942d55c2a77
Siwas666 <seimon656@gmail.com>
2024-05-23 23:34:14 -0600

    Se crea logica para mandar datos al request

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,18 @@
+import com.conversor.Conversor;
+
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        //API key 77c6c01ad7eeeb6ea0e799ff
-        //GET Request API Pair https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP
+        ApiRequest resultConvert = new ApiRequest();
+        Scanner readBase = new Scanner(System.in);
+        Scanner readTarget = new Scanner(System.in);
+        System.out.println("Ingrese el codigo de moneda de la base del tipo de cambio");
+        var baseConvert = readBase.nextLine();
+        System.out.println("Ingrese el codigo a lo que lo desea convertir");
+        var targetConvert = readTarget.nextLine();
+        Conversor convercionBase = resultConvert.convercion(baseConvert,targetConvert);
+        System.out.println(convercionBase.conversion_rate());
+
     }
 }
