fae1b3788f81f60eaa8e9457b2fcb6d92dd5c2ae
Siwas666 <seimon656@gmail.com>
2024-05-24 01:30:08 -0600

    Se enlaza con la clase para generar archivos

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,10 @@
 import com.conversor.Conversor;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApiRequest resultConvert = new ApiRequest();
         Scanner readMenu = new Scanner(System.in);
         String baseConvert;
@@ -28,58 +29,66 @@ public class Principal {
                 case 1:
                     baseConvert = "USD";
                     targetConvert = "ARS";
-                    Conversor conversionAD = resultConvert.conversion(baseConvert,targetConvert);
-                    System.out.println("Conversion de Dolar a Peso Argentino: "+conversionAD.conversion_rate());
-                    // Aquí va el código para la conversión de Dolar a Peso Argentino
+                    Conversor conversionDA = resultConvert.conversion(baseConvert,targetConvert);
+                    System.out.println("Conversion de Dolar a Peso Argentino: "+conversionDA.conversion_rate());
+                    GeneradorArchivos generadorDA = new GeneradorArchivos();
+                    generadorDA.guardarJson(conversionDA);
                     break;
                 case 2:
                     baseConvert = "ARS";
                     targetConvert = "USD";
-                    Conversor conversionDA = resultConvert.conversion(baseConvert,targetConvert);
-                    System.out.println("Conversion de Peso Argentino a Dolar: " + conversionDA.conversion_rate());
-                    // Aquí va el código para la conversión de Peso Argentino a Dolar
+                    Conversor conversionAD = resultConvert.conversion(baseConvert,targetConvert);
+                    System.out.println("Conversion de Peso Argentino a Dolar: " + conversionAD.conversion_rate());
+                    GeneradorArchivos generadorAD = new GeneradorArchivos();
+                    generadorAD.guardarJson(conversionAD);
                     break;
                 case 3:
                     baseConvert = "USD";
                     targetConvert = "BRL";
                     Conversor conversionUB = resultConvert.conversion(baseConvert,targetConvert);
                     System.out.println("Conversion de Dolar a Real Brasileño: " + conversionUB.conversion_rate());
-                    // Aquí va el código para la conversión de Dolar a Real Brasileño
+                    GeneradorArchivos generadorUB = new GeneradorArchivos();
+                    generadorUB.guardarJson(conversionUB);
                     break;
                 case 4:
                     baseConvert = "BRL";
                     targetConvert = "USD";
                     Conversor conversionBD = resultConvert.conversion(baseConvert,targetConvert);
                     System.out.println("Conversion de Real Brasileño a Dolar: " + conversionBD.conversion_rate());
-                    // Aquí va el código para la conversión de Real Brasileño a Dolar
+                    GeneradorArchivos generadorBD = new GeneradorArchivos();
+                    generadorBD.guardarJson(conversionBD);
                     break;
                 case 5:
                     baseConvert = "USD";
                     targetConvert = "COP";
                     Conversor conversionDC = resultConvert.conversion(baseConvert,targetConvert);
                     System.out.println("Conversion de Dolar a Peso Colombiano: " + conversionDC.conversion_rate());
-                    // Aquí va el código para la conversión de Dolar a Peso Colombiano
+                    GeneradorArchivos generadorDC = new GeneradorArchivos();
+                    generadorDC.guardarJson(conversionDC);
                     break;
                 case 6:
                     baseConvert = "COP";
                     targetConvert = "USD";
                     Conversor conversionCD = resultConvert.conversion(baseConvert,targetConvert);
                     System.out.println("Conversion de Peso Colombiano a Dolar: " + conversionCD.conversion_rate());
-                    // Aquí va el código para la conversión de Peso Colombiano a Dolar
+                    GeneradorArchivos generadorCD = new GeneradorArchivos();
+                    generadorCD.guardarJson(conversionCD);
                     break;
                 case 7:
                     baseConvert = "USD";
                     targetConvert = "MXN";
                     Conversor conversionDM = resultConvert.conversion(baseConvert,targetConvert);
                     System.out.println("Conversion de Dolar a Peso Mexicano: " + conversionDM.conversion_rate());
-                    // Aquí va el código para la conversión de Dolar a Peso Mexicano
+                    GeneradorArchivos generadorDM = new GeneradorArchivos();
+                    generadorDM.guardarJson(conversionDM);
                     break;
                 case 8:
                     baseConvert = "MXN";
                     targetConvert = "USD";
                     Conversor conversionMD = resultConvert.conversion(baseConvert,targetConvert);
                     System.out.println("Conversion de Peso Mexicano a Dolar: " + conversionMD.conversion_rate());
-                    // Aquí va el código para la conversión de Peso Mexicano a Dolar
+                    GeneradorArchivos generadorMD = new GeneradorArchivos();
+                    generadorMD.guardarJson(conversionMD);
                     break;
                 case 9:
                     System.out.println("Saliendo del menú.");
