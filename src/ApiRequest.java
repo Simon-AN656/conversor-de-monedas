1af8cd94f774c847256bad414ef39f92ec236130
Siwas666 <seimon656@gmail.com>
2024-05-23 23:33:42 -0600

    Se crea request, response and exception

--- src/ApiRequest.java
+++ src/ApiRequest.java
@@ -1,2 +1,29 @@
+import com.conversor.Conversor;
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ApiRequest {
+    public Conversor convercion (String baseConvert, String targetConvert){
+        URI apiDirection = URI.create("https://v6.exchangerate-api.com/v6/77c6c01ad7eeeb6ea0e799ff/pair/"
+                +baseConvert+"/"+targetConvert);
+        //URI apiDirection = URI.create("https://v6.exchangerate-api.com/v6/77c6c01ad7eeeb6ea0e799ff/latest/"+baseConvert);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.valueOf((apiDirection))))
+                .build();
+        try {
+            HttpResponse<String> response = null;
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Conversor.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontre ese tipo de cambio");
+        }
+    }
+
 }
